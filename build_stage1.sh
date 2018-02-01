655acd3a233b60c5618d0239e9a716865c0b595a
Andreas Baumann <mail@andreasbaumann.cc>
2018-02-01 18:34:42 +0100

    building more utilities

--- build_stage1.sh
+++ build_stage1.sh
@@ -12,7 +12,10 @@ ncurses readline bash joe
 attr acl gmp gdbm db perl openssl
 zlib pambase cracklib libtirpc pam libcap coreutils
 util-linux e2fsprogs
-expat bzip2 lz4 xz pcre less gzip tar libarchive curl"
+expat bzip2 lz4 xz pcre less gzip tar libarchive curl
+
+sed texinfo grep findutils file diffutils ed patch
+kbd procps-ng shadow"
 
 for p in $PACKAGES; do
 	"$SCRIPT_DIR/build_stage1_package.sh" "$p"
